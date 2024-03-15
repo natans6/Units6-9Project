6141889c02deddbe2f22fc7eb39f682c6d4b9611
BT_1N3_25 <sample@bths.edu>
2024-03-15 08:49:06 -0400

    ChoosePhrase

--- ChoosePhrase.java
+++ ChoosePhrase.java
@@ -1,3 +1,33 @@
-public class ChoosePhrase {
+import java.util.ArrayList;
 
+public class ChoosePhrase {
+    private String level;
+
+    public ChoosePhrase()   {
+        level = "easy";
+        listOfPhrases();
+    }
+
+    public void setLevel(String newLevel)  {
+        level = newLevel;
+    }
+
+    public void listOfPhrases() {
+        ArrayList<String> easyLevel = new ArrayList<>();
+        easyLevel.add("Bahamas");
+        easyLevel.add("Japan");
+        easyLevel.add("China");
+        easyLevel.add("Costa Rica");
+        ArrayList<String> mediumLevel = new ArrayList<>();
+        mediumLevel.add("Binary Search");
+        mediumLevel.add("Iteration");
+        mediumLevel.add("Algorithm");
+        mediumLevel.add("Jargon");
+        ArrayList<String> hardLevel = new ArrayList<>();
+        hardLevel.add("Harry Potter");
+        hardLevel.add("Song of Solomon");
+        hardLevel.add("Percy Jackson");
+        hardLevel.add("To a kill a mocking bird");
+
+    }
 }
