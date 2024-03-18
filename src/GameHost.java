1675c37b5613dc7d3a5f87961467a68d4251c744
BT_1N3_25 <sample@bths.edu>
2024-03-18 09:15:53 -0400

    Setting the WheelOfFortunePhrase

--- src/GameHost.java
+++ src/GameHost.java
@@ -1,3 +1,3 @@
-public class GameHost extends Player{
+public class GameHost {
 
 }
--- src/WheelOfFortune.java
+++ src/WheelOfFortune.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+
 public class WheelOfFortune {
     private String[][] WheelOfFortunePhrase;
 
+    private int lives;
     public WheelOfFortune(String phrase){
-        WheelOfFortunePhrase = new String[phrase.length()][3];
+        lives = 3;
+        String[] words = phrase.split(" ");
+        int count = 0;
+        String largestWord = "";
+        for (int j = 0; j < words.length; j++)  {
+            if (words[j].length() > largestWord.length())    {
+                largestWord = words[j];
+            }
+            count++;
+        }
+        WheelOfFortunePhrase = new String[largestWord.length() + 2][count + 2];
     }
 
     // adds a phrase to 2D array
