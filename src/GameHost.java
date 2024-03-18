aa74537d8a055a0b9bb1e73d47474549f23d5482
BT_1N3_23 <sample@bths.edu>
2024-03-18 09:23:47 -0400

    GameHost constructor

--- src/GameHost.java
+++ src/GameHost.java
@@ -1,3 +1,7 @@
 public class GameHost extends Player{
-
+    private String message;
+    public GameHost(String name, String message){
+        super(name);
+        this.message = message;
+    }
 }
